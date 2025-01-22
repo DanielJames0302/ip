240eeed11b9978eabff9eb4625c8c3cdef6729df
TramMinhMan <minhmanuni@gmail.com>
2025-01-22 22:45:46 +0800

    add the ability to mark tasks as done and add the ability to change the status back to not done

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -8,7 +8,7 @@ public class Buddy {
         System.out.println("What can I do for you?");
         System.out.println(border);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -22,11 +22,27 @@ public class Buddy {
                 System.out.println(" Bye. Hope to see you again soon!");
             } else if (command.equals("list")) {
                 for (int i = 0; i < taskCount; i += 1) {
-                    System.out.println(" " + i + ". " +  tasks[i] + "\n");
+                    System.out.println(" " + i + ". " +
+                            tasks[i].toString() + "\n");
+                }
+            } else if (command.startsWith("mark ")) {
+                int task_id = Integer.parseInt(command.split(" ")[1]) - 1;
+                if (task_id >= 0 && task_id <= tasks.length) {
+                    tasks[task_id].markTaskAsDone();
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[task_id].toString());
+                }
+
+            } else if (command.startsWith("unmark ")) {
+                int task_id = Integer.parseInt(command.split(" ")[1]) - 1;
+                if (task_id >= 0 && task_id <= tasks.length) {
+                    tasks[task_id].unmarkTaskAsDone();
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[task_id].toString());
                 }
             } else {
                 System.out.println(" added: " + command);
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount += 1;
             }
 
