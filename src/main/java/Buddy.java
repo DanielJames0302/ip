d626b6616d1f1e6074c0d06ab579c165a198d816
TramMinhMan <minhmanuni@gmail.com>
2025-01-22 22:09:22 +0800

    improve Buddy to echo commands entered by the user and exit when the user types command bye

--- src/main/java/Buddy.java
+++ src/main/java/Buddy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Buddy {
     public static void main(String[] args) {
         String border = "____________________________________________________________";
@@ -5,7 +7,21 @@ public class Buddy {
         System.out.println("Hello! I'm Buddy\n");
         System.out.println("What can I do for you?");
         System.out.println(border);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(border);
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        do {
+            command = scanner.nextLine(); // Read user input
+            if (command.equals("bye")) {
+                System.out.println(border);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(border);
+            } else {
+                System.out.println(border);
+                System.out.println(" " + command); // Echo the command
+                System.out.println(border);
+            }
+
+        } while(!command.equals("bye"));
+        scanner.close();
     }
 }
