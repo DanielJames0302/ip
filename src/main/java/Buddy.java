1b90ca7c3516df8dc1e16feefef27020ada455b4
TramMinhMan <minhmanuni@gmail.com>
2025-01-22 21:56:01 +0800

    add Buddy.java and delete Duke.java

---
+++ src/main/java/Buddy.java
@@ -0,0 +1,11 @@
+public class Buddy {
+    public static void main(String[] args) {
+        String border = "____________________________________________________________";
+        System.out.println(border);
+        System.out.println("Hello! I'm Buddy\n");
+        System.out.println("What can I do for you?");
+        System.out.println(border);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(border);
+    }
+}
