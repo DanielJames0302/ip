4694f67acc8a3903bb6bc2d854ea94f62aac9956
TramMinhMan <minhmanuni@gmail.com>
2025-01-23 21:39:00 +0800

    Add javadoc comments for Task.java and TaskList.java

--- src/main/java/task/Task.java
+++ src/main/java/task/Task.java
@@ -4,24 +4,46 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Constructor for Task class.
+     *
+     * @param description description of a task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns string representation of the task status.
+     *
+     * @return Lateral location.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Mark the task as done.
+     *
+     */
     public void markTaskAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Mark the task as not done.
+     *
+     */
     public void unmarkTaskAsDone() {
         this.isDone = false;
     }
 
-
+    /**
+     * Returns string representation of a task.
+     *
+     * @return string representation of a task.
+     */
     public String toString() {
         return "[" + getStatusIcon() + "] " + this.description;
     }
--- src/main/java/task/TaskList.java
+++ src/main/java/task/TaskList.java
@@ -9,22 +9,47 @@ import java.util.ArrayList;
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Constructor for TaskList class.
+     *
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Add new task to current task list.
+     *
+     * @param task the new task.
+     */
     public void addTask(Task task) {
         this.tasks.add(task);
     }
 
+    /**
+     * Returns the size of task list.
+     *
+     * @return the size of task list.
+     */
     public int getLength() {
         return this.tasks.size();
     }
 
+    /**
+     * Returns task indicated by index.
+     *
+     * @param index task index in the task list.
+     * @return task indicated by index.
+     */
     public Task getTask(int index) {
         return this.tasks.get(index);
     }
 
+    /**
+     * Deletes taskToDelete in the task list
+     *
+     * @param taskToDelete task that needs to be deleted.
+     */
     public void deleteTask(Task taskToDelete) {
         this.tasks = this.tasks.stream()
                 .filter(task -> !task.equals(taskToDelete) )
