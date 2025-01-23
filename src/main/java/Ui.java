f2eebafba9e34912fa8d6f0373ae95cb65e6f945
TramMinhMan <minhmanuni@gmail.com>
2025-01-23 19:34:43 +0800

    test using the I/O redirection technique and fix ui responses

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -4,14 +4,14 @@ import task.TaskList;
 import java.util.Scanner;
 
 public class Ui {
-    private static final String BORDER = "\n____________________________________________________________\n";
+    private static final String BORDER = "____________________________________________________________";
     private final Scanner scanner = new Scanner(System.in);
 
 
     public static String greet() {
         return String.format(Ui.BORDER + "\n" +
-                "Hello! I'm Buddy\n" + "What can I do for you?" +
-                "\n" + Ui.BORDER);
+                "Hello! I'm Buddy\n" + "What can I do for you?\n" +
+                Ui.BORDER);
     }
 
     public String readInput() {
@@ -29,24 +29,24 @@ public class Ui {
     }
 
     public static String markTask(Task task) {
-        return Ui.BORDER + " Nice! I've marked this task as done:\n"
-                + String.format(" %s", task.toString()) + Ui.BORDER;
+        return Ui.BORDER + "\n Nice! I've marked this task as done:\n"
+                + String.format(" %s\n", task.toString()) + Ui.BORDER;
     }
 
     public static String unmarkTask(Task task) {
-        return Ui.BORDER + " OK, I've marked this task as not done yet:\n"
-                + String.format(" %s", task.toString()) + Ui.BORDER;
+        return Ui.BORDER + "\n OK, I've marked this task as not done yet:\n"
+                + String.format(" %s\n", task.toString()) + Ui.BORDER;
     }
 
     public static String addTask(Task task, TaskList taskList) {
-        return Ui.BORDER + String.format("  Got it. I've added this task: \n  %s",
+        return Ui.BORDER + String.format("\n  Got it. I've added this task: \n  %s",
                 task.toString()) +
-                String.format("\n  Now you have %s tasks in the list.", taskList.getLength())
+                String.format("\n  Now you have %s tasks in the list.\n", taskList.getLength())
                 + Ui.BORDER;
     }
 
     public static String listTasks(TaskList taskList) {
-        String result = Ui.BORDER + "  Here are the tasks in your list:\n";
+        String result = Ui.BORDER + "\n  Here are the tasks in your list:\n";
         for (int i = 0; i < taskList.getLength(); i += 1) {
             result = result + String.format("  %s.%s\n", i + 1,
                     taskList.getTask(i).toString());
