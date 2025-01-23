066b2aee2cd9a190047e2160a740a7b9732a594b
TramMinhMan <minhmanuni@gmail.com>
2025-01-23 21:30:46 +0800

    Add javadoc comments for Event.java

--- src/main/java/task/Event.java
+++ src/main/java/task/Event.java
@@ -4,12 +4,24 @@ public class Event extends Task{
     protected String from;
     protected String to;
 
+    /**
+     * Constructor for Event class.
+     *
+     * @param description description of the Event task.
+     * @param from Y start time of the Event task.
+     * @param to end time of the Event task.
+     */
     public Event(String description, String from, String to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Returns string representation of Event task.
+     *
+     * @return string representation of Event task.
+     */
     @Override
     public String toString() {
         return "[E]" + super.toString() +
