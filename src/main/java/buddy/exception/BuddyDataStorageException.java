525700da147e0bf5da92ba9f52bed78d15f475fe
TramMinhMan <minhmanuni@gmail.com>
2025-01-30 22:46:40 +0800

    Add javadoc comments for BuddyDataStorageException.java

--- src/main/java/buddy/exception/BuddyDataStorageException.java
+++ src/main/java/buddy/exception/BuddyDataStorageException.java
@@ -1,11 +1,16 @@
 package buddy.exception;
 
 public class BuddyDataStorageException extends BuddyException {
-
+    
     public BuddyDataStorageException(String message) {
         super(message);
     }
 
+    /**
+     * Retrieve information string of the exception.
+     *
+     * @return information string of the exception.
+     */
     @Override
     public String toString() {
         return super.toString() + " " + this.message;
